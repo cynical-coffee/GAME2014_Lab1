b089c858161513a8785d78c3d2dfcd69dd3d0ebd
Meghan <mparis003@gmail.com>
2021-09-15 23:28:51 -0400

    Created project files.

---
+++ Assets/[Scripts]/StartButtonBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButtonBehaviour : MonoBehaviour
+{
+    public void OnStartButtonPress()
+    {
+        
+    }
+}
